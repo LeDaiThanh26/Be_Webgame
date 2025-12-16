5888f06ebc2d6da110232197e05bcb488a597744
Nguyễn Viết Lâm Phong <lamphongnet1@gmail.com>
2025-12-16 13:40:54 +0700

    add Favourite Model and Recent Model

---
+++ src/models/Recent.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const recentSchema = new mongoose.Schema({
+  id_user: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', 
+    required: true 
+  },
+  id_game: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Game', 
+    required: true 
+  }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Recent', recentSchema)
