2a16d550912949a18359a3df18055d1ee29897c0
Nguyễn Viết Lâm Phong <lamphongnet1@gmail.com>
2025-12-16 17:41:13 +0700

    fix bug favourite

--- src/routes/favourite.route.js
+++ src/routes/favourite.route.js
@@ -4,8 +4,6 @@ const controller = require('../controllers/favourite.controller')
 router.post('/', controller.createFavourite)
 router.get('/', controller.getAllFavourites)
 router.get('/user/:userId', controller.getFavouritesByUser)
-router.get('/:id', controller.getFavouriteById)
-router.delete('/:id', controller.deleteFavourite)
 router.delete('/user/:userId/game/:gameId', controller.deleteFavouriteByUserAndGame)
 
 module.exports = router
