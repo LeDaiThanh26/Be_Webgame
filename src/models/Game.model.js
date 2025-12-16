815879b02b27bad5d2ea9c1e5f67132357538788
Nguyễn Viết Lâm Phong <lamphongnet1@gmail.com>
2025-12-16 13:29:18 +0700

    Create Game Model

---
+++ src/models/Game.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const gameSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  dev: String,
+  release_date: Date,
+  last_update: { type: Date, default: Date.now },
+  technology: String,
+  platforms: [String],
+  category: String,
+  thumbnail: String,
+  description: String,
+  image: [String],
+  link_game: String
+}, { timestamps: true })
+
+module.exports = mongoose.model('Game', gameSchema)
