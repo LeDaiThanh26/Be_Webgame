7ec8e32d560f23fa0ecc9df6460524f3395a981b
Le Dai Thanh <thanh261220@gmail.com>
2025-12-17 15:22:30 +0700

    done leaderboard

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -40,3 +40,64 @@ exports.getMe = async (req, res) => {
   const user = await User.findById(req.userId).select('-password')
   res.json(user)
 }
+
+//lay tat ca user
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find().select('-[password')
+    res.json(users)
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching users', error: error.message })
+  }
+}
+
+//cong diem tu game da click
+exports.addPoints = async (req, res) => {
+  try {
+    const { userId, playSeconds } = req.body
+    console.log("ADD POINT:", userId, playSeconds);
+
+    if (!userId) {
+      return res.status(400).json({ message: 'User ID is required' })
+    }
+
+    // Chưa đủ 60s → không cộng gì
+    // if (!playSeconds || playSeconds < 60) {
+    //   return res.json({
+    //     success: true,
+    //     xpAdded: 0
+    //   })
+    // }
+
+    const user = await User.findById(userId)
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
+    /* ===== XP CALC ===== */
+    const minutes = Math.floor(playSeconds / 60)
+
+    let xp = minutes * 5
+
+    if (minutes >= 5) {
+      xp += 10
+    }
+
+    xp = Math.min(xp, 60)
+
+    /* ===== UPDATE USER ===== */
+    user.experiencePoints += xp
+    user.playTime += playSeconds
+
+    await user.save()
+
+    res.json({
+      success: true,
+      xpAdded: xp,
+      totalXp: user.experiencePoints,
+      totalPlayTime: user.playTime
+    })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,6 +4,7 @@ const auth = require('../middlewares/auth.middleware')
 
 router.post('/register', controller.register)
 router.post('/login', controller.login)
+router.post('/addpoint', controller.addPoints)
 router.get('/me', auth, controller.getMe)
-
+router.get('/', controller.getAllUsers)
 module.exports = router
